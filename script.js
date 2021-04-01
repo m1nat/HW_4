6335023fa24510ea70a80c8d84b6d0fa7d61d0ea
unknown <you@example.com>
2021-04-01 21:30:53 +0300

    cw14

---
+++ script.js
@@ -0,0 +1,82 @@
+// const arr = [1, 'hello'];
+// console.log(arr);
+// arr.push(60);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.unshift('IT');
+// console.log(arr);
+
+// // const matrix = [
+// //     [1, 2, 3],
+// //     [4, 5, 6],
+// //     [7, 8, 9]
+// // ];
+// // console.log(matrix[2][2]);
+
+// // const arr2 = new Array(4, 4, 4);
+// // console.log(arr2);
+
+// let str = 'helo!';
+// console.log(str);
+// str = str.split('');
+// console.log(str);
+// str = str.join('')
+// console.log(str);
+
+
+// console.log(arr.toString());
+
+// arr.splice(0, 0);
+
+
+
+
+// const arr1 = arr.slice(0, 2);
+// const arr2 = arr.slice(2, 4);
+// console.log(arr);
+// console.log(arr1);
+// console.log(arr2);
+
+// const arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+// let arr = ['hello', 'world'];
+
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i].toUpperCase();
+// }
+
+// console.log(arr);
+
+// const arr = [5, 67, 89, 43, 32, 43, 65];
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     let f = i % 2;
+//     if (f == 1) {
+//         console.log(arr[i]);
+//     }
+// }
+
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i] 
+// }
+
+// console.log(sum);
+
+
+const arr = [67, 89, 43, 32, 43, 65];
+
+for (i = 0; i < arr.length; i++) {
+    
+    if (arr[i] > minMax) {
+        minMax = arr[i]
+    }    
+}
+
+console.log(minMax);
